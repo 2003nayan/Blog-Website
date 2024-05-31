7a7758f6d0602c157929983e8ba302cf3042b6a5
Lucifer09 <nayankatiyara123@gmail.com>
2024-05-31 07:21:29 +0530

    Decided the name of the website and the logo too

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Navbar } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <Navbar className="border-b-2">
+      <Link to="/">
+        <img src="client/src/assets/logo-no-background.png" />
+      </Link>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
