5785d53f06d37bc3f9d0da584dc2bb30c3771a60
Lucifer09 <nayankatiyara123@gmail.com>
2024-06-01 17:32:46 +0530

    Completed the header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,48 @@
-import { Navbar, TextInput } from "flowbite-react";
+import { Button, Navbar, TextInput } from "flowbite-react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <Navbar className="border-b-2 bg-black">
+    <Navbar className="border-b-2">
       <Link
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <img className="h-10" src="/public/logo-no-background.png" alt="Logo" />
+        <img className="h-10" src="/public/black-logo.png" alt="Logo" />
       </Link>
       <form>
         <TextInput
           type="text"
           placeholder="Search"
           rightIcon={AiOutlineSearch}
+          className="hidden lg:inline "
         />
       </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+        </Link>
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
