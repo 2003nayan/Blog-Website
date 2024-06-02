786b252f70e376db3c9f4d967d5d4f3587e9802b
Lucifer09 <nayankatiyara123@gmail.com>
2024-06-02 15:02:04 +0530

    Added dark mode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Button, Navbar, NavbarToggle, TextInput } from "flowbite-react";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 
-const Header = () => {
+export default function Header() {
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <Link
@@ -29,22 +30,21 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
         </Link>
+        <NavbarToggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/"} as={'div'}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={'div'}>
           <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={'div'}>
           <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
-};
-
-export default Header;
+}
