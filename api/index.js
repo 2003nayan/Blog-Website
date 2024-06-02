1688b43381245f1fc85a0c49bc6af02b579a83aa
Lucifer09 <nayankatiyara123@gmail.com>
2024-06-02 15:43:43 +0530

    Connected server to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
-import { log } from "console";
+import mongoose from "mongoose";
 import express from "express";
+
+mongoose.connect('mongodb+srv://nayankatiyara:Nayan@2003@Cluster-Blog-website.9iulijo.mongodb.net/Blog-website?retryWrites=true&w=majority')
+    .then(() => {
+        console.log("DB is connected");
+    }).catch(err => {
+        console.log(err);
+    })
 
 const app = express();
 
