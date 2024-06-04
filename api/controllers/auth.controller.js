78607cf7e5330691cd11965ccdc8a689d0807960
Lucifer09 <nayankatiyara123@gmail.com>
2024-06-04 15:58:31 +0530

    Added a middleware to handle wrrors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,12 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utilis/error.js';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({
-            message: "Please fill all the fields"
-        })
+        next(errorHandler(400, "All fields are required"))
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -18,6 +17,10 @@ export const signup = async (req, res, next) => {
         hashedPassword
     })
 
-    await newUser.save();
-    res.json("Signup successful")
+    try {
+        await newUser.save();
+        res.json("Signup successful")
+    } catch (error) {
+        next(error);
+    }
 }
